a6223ef45ce67c06c68c0255dcaf62b75e8b9737
Adam Jetmalani <adam.k.jet@gmail.com>
2013-05-19 15:01:13 -0700

    2 done

--- projects/all_stats_test_run/main.rb
+++ projects/all_stats_test_run/main.rb
@@ -2,6 +2,12 @@ load '../../environment.rb'
 
 users = @data.network.values.flatten.collect(&:user)
 fields = users.inject(Hashie::Mash.new) do |collection, user| 
-  user.each {|field, value| (collection[field] ||= []) << value}
+  user.each { |field, value| (collection[field] ||= []) << value }
   collection
 end
+
+stats = fields.keep_if { |key, arr| arr.first.is_a? Numeric }
+output = stats.update(stats) { |key, arr| arr.all_stats}.to_json
+f = File.new(File.dirname(__FILE__) + '/output/all_stats_test_run.json', "w")
+f.write(output)
+f.close
