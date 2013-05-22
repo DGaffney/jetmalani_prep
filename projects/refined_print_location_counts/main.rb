4805f849c680fe9d19328b1466f38397d8974262
Adam Jetmalani <adam.k.jet@gmail.com>
2013-05-22 01:47:36 -0700

    bedtime

--- projects/refined_print_location_counts/main.rb
+++ projects/refined_print_location_counts/main.rb
@@ -1,14 +1,23 @@
 load '../../environment.rb'
-Geocoder::Configuration.timeout = 10
+Geocoder.configure(:lookup => :yandex)
 
-unformatted_counts = @data.network.values.flatten.collect(&:user).collect(&:location).counts
-formatted_counts = Hash.new(0)
+users = @data.network.values.flatten.collect(&:user)
+cache = {}
 
-unformatted_counts.each do |location, count|
-  geo = Geocoder.search(location)
-  if geo
-    formatted_counts[geo] += count
+users.each do |user|
+  initial = user.location
+  
+  if cache.has_key?(initial)
+    tgeo = cache[initial]
   else
-    formatted_counts[location] = count
+    tgeo = Geocoder.search(initial).first
+    cache[initial] = tgeo
   end
+
+  user.geo = tgeo
 end
+
+output = users.collect(&:geo).counts.to_json
+f = File.new(File.dirname(__FILE__) + '/output/location_counts_refined.json', "w")
+f.write(output)
+f.close
