904723cc01369811e9a5cc95274cae636ba9fdd3
Adam Jetmalani <adam.k.jet@gmail.com>
2013-05-21 21:31:47 -0700

    not done

--- projects/refined_print_location_counts/main.rb
+++ projects/refined_print_location_counts/main.rb
@@ -1,4 +1,14 @@
 load '../../environment.rb'
-Geocoder.configure(:lookup => :bing, :api_key => BingKey::BING_IT)
+Geocoder::Configuration.timeout = 10
 
-l_s_counts = @data.network.values.flatten.collect(&:user).collect(&:location).counts
+unformatted_counts = @data.network.values.flatten.collect(&:user).collect(&:location).counts
+formatted_counts = Hash.new(0)
+
+unformatted_counts.each do |location, count|
+  geo = Geocoder.search(location)
+  if geo
+    formatted_counts[geo] += count
+  else
+    formatted_counts[location] = count
+  end
+end
